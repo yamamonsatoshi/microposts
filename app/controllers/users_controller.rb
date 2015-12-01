333970afc3cd984c0f9046ab94671a4b7643d10b
Satoshi Yamauchi <perfect.wanderer@gmail.com>
2015-12-01 15:46:28 +0000

    added tweet function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   # GET /users/1
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.order(created_at: :desc)
   end
   
   # GET /users/new
